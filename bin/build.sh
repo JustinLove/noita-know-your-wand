37c9d3dbad34cf9bd684ac4fad5275c0dce7a61c
Justin Love <git@JustinLove.name>
2022-06-20 10:54:22 -0500

    compressed javascript

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-elm make src/NoitaKnowYourWand.elm --optimize --output public/noita-know-your-wand.js
+elm make src/NoitaKnowYourWand.elm --optimize --output public/noita-know-your-wand.max.js
+cd tools
+npx esbuild ../public/noita-know-your-wand.max.js --minify --target=es5 --outfile=../public/noita-know-your-wand.js
+rm ../public/noita-know-your-wand.max.js
